af5bc165e51ef911225b6f7b924ccdc6ebb0a332
jihun8428 <tony3600@naver.com>
2022-12-11 18:35:21 +0900

    3일차 커밋

--- PROGRAMMERS/level_0_연속된 수의 합.py
+++ PROGRAMMERS/level_0_연속된 수의 합.py
@@ -1,11 +1,7 @@
 def solution(num, total):
-    answer = []
-    for i in range(num):
-        answer.append(0)
-        for j in range(num):
-            answer[j] += 1
+    average = total // num
+    return [i for i in range(average - (num-1)//2, average + (num + 1)//2)]
 
-    return answer
 num = int(input())
 total = int(input())
 print(solution(num, total))
---
+++ PROGRAMMERS/level_0_종이 자르기.py
@@ -0,0 +1,8 @@
+def solution(M, N):
+    answer = 0
+    answer = (M * N) - 1
+    return answer
+
+M = int(input())
+N = int(input())
+print(solution(M, N))
